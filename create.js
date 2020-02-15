72ed681d5ebe918b9768fea51d4f01d62557f556
Jack Pattison <jepatt16@stlawu.edu>
2020-02-15 17:49:26 -0500

    election history

--- create.js
+++ create.js
@@ -6,6 +6,21 @@ const connect = require('./db.js');
 const Voter = require('./schema.js');
 const fs = require('fs');
 
+// election object, constructs from a string of length 4
+const Election = function(s){
+	this.type = s.slice(0, 2);
+	this.year = parseInt(s.slice(2));
+};
+
+// helper function to make an array of elections from a string
+const mkhistory = function(s){
+	let elections = [];
+	for (let i = 0; i < s.length; i += 4){
+		elections.push(new Election(s.slice(i, i + 4)));
+	}
+	return elections;
+};
+
 // read from file and make array
 let voters = [];
 fs.readFile('voters.csv', function(error, data){
@@ -14,16 +29,17 @@ fs.readFile('voters.csv', function(error, data){
 	const data_arr = data.toString().split('\r\n');
 
 	for (const line of data_arr){
-		const line_arr = line.split(',');
-		const a_voter = new Voter({
-			first: line_arr[0],
-			last: line_arr[1],
-			zip: parseInt(line_arr[2]),
-			history : []
+		if (line){
+			const line_arr = line.split(',');
+			const a_voter = new Voter({
+				first: line_arr[0],
+				last: line_arr[1],
+				zip: parseInt(line_arr[2]),
+				history : mkhistory(line_arr[3])
 			});
-		voters.push(a_voter);
+			voters.push(a_voter);
+		}
 	}
-	console.log(voters);
 });
 
 //connect(); // database time
