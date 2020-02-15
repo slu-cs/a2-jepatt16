5728208b721a1ba1863f24a2b1c994db81567747
Jack Pattison <jepatt16@stlawu.edu>
2020-02-15 17:29:49 -0500

    array of voters

--- create.js
+++ create.js
@@ -7,10 +7,23 @@ const Voter = require('./schema.js');
 const fs = require('fs');
 
 // read from file and make array
+let voters = [];
 fs.readFile('voters.csv', function(error, data){
 	if (error) console.error(error.stack);
-	console.log(data.toString());
+	// get file into workable format
+	const data_arr = data.toString().split('\r\n');
 
+	for (const line of data_arr){
+		const line_arr = line.split(',');
+		const a_voter = new Voter({
+			first: line_arr[0],
+			last: line_arr[1],
+			zip: parseInt(line_arr[2]),
+			history : []
+			});
+		voters.push(a_voter);
+	}
+	console.log(voters);
 });
 
 //connect(); // database time
