3d8847096f91adc54491ae4b6753b46a1c60bb9a
jepatt16 <jepatt16@gmail.com>
2020-02-23 17:17:01 -0500

    wrote a couple queries

--- queries.js
+++ queries.js
@@ -8,20 +8,22 @@ connect();
 // queries
 const queries = [
   // How many registered voters live in the Canton zip code (13617)?
-	Voter.find(),
+	Voter.Voter.find().where('zip').equals(13617),
   // What are the full names of all the registered voters whose first-name is STARR?
-  Voter.find().where('first').equals('STARR'),
+  Voter.Voter.find().where('first').equals('STARR'),
 
   // How many people voted in the 2016 general election (GE16)?
+
   // What is the last-name that comes last in the county in alphabetical order?
   // How many zip codes does the county contain?
 ]
 
 Promise.all(queries)
   .then(function(results){
-	console.log('All voters: ', results[0])
-    console.log('Voter\'s whose first name is "STARR": ', results[1])
-    mg.connection.close();
+		console.log('Number of voters in Canton zip: ', results[0].length)
+    console.log('Voter\'s whose first name is "STARR": ', results[1].map(voter => (voter.first + ' ' + voter.last)))
+		//console.log('How many people voted in the 2016 general election: ', )
+		mg.connection.close();
 	})
 	.catch(function(error) {
     console.error(error.stack);
