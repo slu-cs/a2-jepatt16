65fdeb65932c541bc72c24df6fbcf6465e710feb
jepatt16 <jepatt16@gmail.com>
2020-02-23 18:12:09 -0500

    on the mysterious absence of the letter 'n'

--- queries.js
+++ queries.js
@@ -12,13 +12,19 @@ const queries = [
   // What are the full names of all the registered voters whose first-name is STARR?
   Voter.find().where('first').equals('STARR'),
   // How many people voted in the 2016 general election (GE16)?
-	Voter.find(),
   // What is the last-name that comes last in the county in alphabetical order?
 	Voter.find().sort({'last' : -1}),
 	// How many zip codes does the county contain?
 	Voter.distinct('zip')
 ]
 
+// I have no idea why, but the letter 'n' does not appear anywhere in the file.
+// It appears that it has been removed from several places, leaving behind
+// names such as "Ackerma" (presumably originally "Ackerman") or,
+// even more suspiciously, "Ewma" (almost certainly a corruption of "Newman")
+// This is a problem with the file itself (or at least my copy of it), and not
+// the way I am reading the file in create.js. I know this because this quirk
+// is present in the original voters.csv file when I view in in nano
 Promise.all(queries)
   .then(function(results){
 		console.log('Number of voters in Canton zip: ', results[0].length);
@@ -27,9 +33,9 @@ Promise.all(queries)
 
 		console.log('How many people voted in the 2016 general election: ', results[2].map(voter => voter.history).filter(history => history.includes('GE16')).length);
 
-		console.log('Last name in the phonebook: ', results[3][0].last);
+		console.log('Last name in the phonebook: ', results[2][0].last);
 
-		console.log('Number of zip codes: ', results[4].length)
+		console.log('Number of zip codes: ', results[3].length)
 
 		mg.connection.close();
 	})
